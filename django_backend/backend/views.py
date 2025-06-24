cbcf6a875d6aa9df1028baeb51cfdc48329f5033
Leona-Wang <leonawang920913@gmail.com>
2025-06-24 15:03:58 +0800

    250624

--- django_backend/backend/views.py
+++ django_backend/backend/views.py
@@ -7,9 +7,16 @@ import json
 # views.py
 @csrf_exempt
 def testApi(request):
+    """sumary_line
+    把前端傳回數字*2的結果
+    Keyword arguments:
+    argument -- description
+    Return: 乘以2的結果
+    """
     if request.method == 'POST':
         data = json.loads(request.body)
         number = data.get('number', 0)
         result = number * 2
         return JsonResponse({'result': result})
     return JsonResponse({'error': 'Invalid request'}, status=400)
+
